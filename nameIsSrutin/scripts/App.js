1d9dabc474957e966325b271f78b9117b35cb711
srutinreddy <srutinreddy@yahoo.com>
2016-08-01 00:44:04 -0700

    Add project files.

---
+++ nameIsSrutin/scripts/App.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('myApp', ["ngRoute"]);
+app.controller('validateCtrl', function ($scope) {
+    
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/page2", {
+    templateUrl: "HtmlPage2.html"
+})
+    .when("/page3", {
+    templateUrl: "HtmlPage3.html"
+})
+    .when("/page1", {
+    templateUrl: "HtmlPage1.html"
+});
+});
